8058c4cbd83fa3728419ef403dcd3b95854039db
REX980 <logwire05@gmail.com>
2025-07-25 12:52:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Currently no advanced JS needed, but this file is here for future enhancements.
+
+// Optional: Smooth scroll on internal links if you add any later.
+// Example:
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//   anchor.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
